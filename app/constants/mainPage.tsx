b06073724e1f56165698ac324f031170dd37d043
ShathaAlrayyani <shatha.alrayyani223@gmail.com>
2024-08-17 03:21:06 +0300

    fixing build

--- app/constants/mainPage.tsx
+++ app/constants/mainPage.tsx
@@ -1,10 +1,11 @@
 
-import CameraIcon from '../assets/cameraIcon.svg'
+import { AboutUsSection, IntroductionSection, KeyServicesSection, MissionVisionSection, TeamCompanySection } from '../components'
 import ActionIcon from '../assets/actionIcon.svg'
-import ServiceIcon from '../assets/servicesIcon.svg'
+import CameraIcon from '../assets/cameraIcon.svg'
 import FilmIcon from '../assets/filmWithPlay.svg'
-import PicturesIcon from '../assets/picturesIcon.svg'
 import PicIcon from '../assets/tvWithPeople.svg'
+import PicturesIcon from '../assets/picturesIcon.svg'
+import ServiceIcon from '../assets/servicesIcon.svg'
 
 
 export const keyServices = [
@@ -38,5 +39,23 @@ export const keyServices = [
     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
     icon: PicturesIcon,
     title: "Motion Graphics",
+  },
+];
+
+export const sectionsElements = [
+  {
+    Component: <IntroductionSection />,
+  },
+  {
+    Component: <AboutUsSection />,
+  },
+  {
+    Component: <MissionVisionSection />,
+  },
+  {
+    Component: <KeyServicesSection />,
+  },
+  {
+    Component: <TeamCompanySection />,
   },
 ];
--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,19 @@
 "use client";
-import { Fragment, useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import { Fragment } from "react";
 import {
-  AboutUsSection,
-  AppSlider,
   Header,
-  IntroductionSection,
-  KeyServicesSection,
-  MissionVisionSection,
-  TeamCompanySection,
 } from "./components";
-import { SwiperSlide } from "swiper/react";
-
-export const sectionsElements = [
-  {
-    component: <IntroductionSection />,
-  },
-  {
-    component: <AboutUsSection />,
-  },
-  {
-    component: <MissionVisionSection />,
-  },
-  {
-    component: <KeyServicesSection />,
-  },
-  {
-    component: <TeamCompanySection />,
-  },
-];
+import { sectionsElements } from "./constants/mainPage";
 
 export default function Home() {
-  useEffect(() => {
-    AOS.init({
-      easing: "ease-in-cubic",
-      offset: 50,
-      disableMutationObserver: true, // Optional: Disable auto-application of styles
-      startEvent: "DOMContentLoaded",
-    });
-    AOS.refresh();
-  }, []);
 
   return (
     <main>
       <Header acivePage="Home" />
       <main className="appWrapper">
         {sectionsElements.map((section, index) => (
-          <Fragment key={index + 123}>{section.component}</Fragment>
+          <Fragment key={index + 123}>{section.Component}</Fragment>
         ))}
-        {/* <AppSlider>
-          {sectionsElements.map((section, i) => (
-            <SwiperSlide key={`section-${i}`}>{section.component}</SwiperSlide>
-          ))}
-        </AppSlider> */}
       </main>
     </main>
   );
